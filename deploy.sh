f0296a7845bb3f24b321f3d7cda86f976dc5b871
zhou-lingyun <1136075877@qq.com>
2021-10-07 18:43:02 +0800

    push

--- deploy.sh
+++ deploy.sh
@@ -14,12 +14,12 @@ cd docs/.vuepress/dist
 
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
-  githubUrl=git@github.com:Shanyaliux/Shanyaliux.github.io.git
+  githubUrl=git@github.com:zhou-lingyun/zhou-lingyun.github.io.git
 else
   msg='来自github actions的自动部署'
-  githubUrl=https://Shanyaliux:${GITHUB_TOKEN}@github.com/Shanyaliux/Shanyaliux.github.io.git
-  git config --global user.name "Shanyaliux"
-  git config --global user.email "445502929@qq.com"
+  githubUrl=https://zhou-lingyun:${GITHUB_TOKEN}@github.com/zhou-lingyun/zhou-lingyun.github.io.git
+  git config --global user.name "zhou-lingyun"
+  git config --global user.email "1136075877@qq.com"
 fi
 git init
 git add -A
@@ -31,9 +31,9 @@ git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
 # echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
 
 # if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-#   codingUrl=git@e.coding.net:shanyaliux/blog/Blog.git
+#   codingUrl=git@e.coding.net:zhou-lingyun/blog/Blog.git
 # else
-#   codingUrl=https://pt24xdu50deu:${CODING_TOKEN}@e.coding.net/shanyaliux/blog/Blog.git
+#   codingUrl=https://pt24xdu50deu:${CODING_TOKEN}@e.coding.net/zhou-lingyun/blog/Blog.git
 # fi
 # git add -A
 # git commit -m "${msg}"
